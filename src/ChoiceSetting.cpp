0b919aa607a9b394f0734ab093c8de38ce105e26
Khronophobia <46954143+Khronophobia@users.noreply.github.com>
2024-03-03 18:34:16 +0700

    Some stuff

--- src/ChoiceSetting.cpp
+++ src/ChoiceSetting.cpp
@@ -1,2 +1,74 @@
 #include <Geode/Geode.hpp>
 #include "ChoiceSetting.hpp"
+
+bool ChoiceSettingValue::load(matjson::Value const& json) {
+    return true;
+}
+
+bool ChoiceSettingValue::save(matjson::Value& json) const {
+    return true;
+}
+
+SettingNode* ChoiceSettingValue::createNode(float width) {
+    return ChoiceSettingNode::create(this, width);
+}
+
+bool ChoiceSettingNode::init(ChoiceSettingValue* value, float width) {
+    if (!SettingNode::init(value)) return false;
+
+    this->setContentSize({width, 40.f});
+    m_name = Mod::get()->getSettingDefinition(value->getKey())->get<CustomSetting>()->json->get<std::string>("name");
+    m_description = Mod::get()->getSettingDefinition(value->getKey())->get<CustomSetting>()->json->get<std::string>("description");
+    auto name = CCLabelBMFont::create(m_name.c_str(), "bigFont.fnt");
+    name->setAnchorPoint({0.f, 0.5f});
+    name->setScale(0.5f);
+    name->setPosition(20.f, this->getContentHeight() / 2.f);
+    this->addChild(name);
+
+    auto infoSpr = CCSprite::createWithSpriteFrameName("GJ_infoIcon_001.png");
+    infoSpr->setScale(0.6f);
+    auto infoBtn = CCMenuItemSpriteExtra::create(
+        infoSpr,
+        this,
+        menu_selector(ChoiceSettingNode::onInfo)
+    );
+    auto infoMenu = CCMenu::create();
+    infoMenu->setPosition(
+        name->getContentWidth() / 2.f + 35.f,
+        this->getContentHeight() / 2
+    );
+    infoMenu->addChild(infoBtn);
+    this->addChild(infoMenu);
+
+    return true;
+}
+
+void ChoiceSettingNode::commit() {}
+
+bool ChoiceSettingNode::hasUncommittedChanges() {
+    return false;
+}
+
+bool ChoiceSettingNode::hasNonDefaultValue() {
+    return true;
+}
+
+void ChoiceSettingNode::resetToDefault() {}
+
+void ChoiceSettingNode::onInfo(CCObject* sender) {
+    FLAlertLayer::create(
+        m_name.c_str(),
+        m_description.c_str(),
+        "OK"
+    )->show();
+}
+
+ChoiceSettingNode* ChoiceSettingNode::create(ChoiceSettingValue* value, float width) {
+    auto ret = new ChoiceSettingNode();
+    if (ret && ret->init(value, width)) {
+        ret->autorelease();
+        return ret;
+    }
+    CC_SAFE_DELETE(ret);
+    return nullptr;
+}
--- src/ChoiceSetting.hpp
+++ src/ChoiceSetting.hpp
@@ -5,8 +5,11 @@
 using namespace geode::prelude;
 
 class ChoiceSettingValue : public SettingValue {
-    std::string m_choice;
+protected:
+    int m_choice;
 public:
+    ChoiceSettingValue(std::string const& key, std::string const& modID, int const& choice)
+        : SettingValue(key, modID), m_choice(choice) {}
     bool load(matjson::Value const& json) override;
     bool save(matjson::Value& json) const override;
     SettingNode* createNode(float width) override;
@@ -14,10 +17,15 @@ public:
 
 class ChoiceSettingNode : public SettingNode {
 protected:
+    std::vector<std::string> m_choices;
+    std::string m_name;
+    std::string m_description;
     bool init(ChoiceSettingValue* value, float width);
 public:
     void commit() override;
     bool hasUncommittedChanges() override;
     bool hasNonDefaultValue() override;
+    void resetToDefault() override;
+    void onInfo(CCObject*);
     static ChoiceSettingNode* create(ChoiceSettingValue* value, float width);
 };
