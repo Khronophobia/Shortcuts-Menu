0d21ea4a8924388f47a0340fc2068f1a02aefa09
Khronophobia <46954143+Khronophobia@users.noreply.github.com>
2024-03-06 16:54:07 +0700

    Convert indentation to space

--- src/main.cpp
+++ src/main.cpp
@@ -10,108 +10,108 @@
 using namespace geode::prelude;
 
 class $modify(MenuLayer) {
-	bool init() {
-		if (!MenuLayer::init()) return false;
+    bool init() {
+        if (!MenuLayer::init()) return false;
 
-		if (Mod::get()->getSettingValue<bool>("enable-menulayer")) {
-			auto shortcutButton = ShortcutsLayer::addShortcutButton(
-				this, 1.f, CircleBaseColor::Green, CircleBaseSize::MediumAlt
-			);
-			auto menu = this->getChildByID("bottom-menu");
-			menu->addChild(shortcutButton);
-			menu->updateLayout();
-		}
+        if (Mod::get()->getSettingValue<bool>("enable-menulayer")) {
+            auto shortcutButton = ShortcutsLayer::addShortcutButton(
+                this, 1.f, CircleBaseColor::Green, CircleBaseSize::MediumAlt
+            );
+            auto menu = this->getChildByID("bottom-menu");
+            menu->addChild(shortcutButton);
+            menu->updateLayout();
+        }
 
-		return true;
-	}
+        return true;
+    }
 };
 
 class $modify(EditLevelLayer) {
-	bool init(GJGameLevel* level) {
-		if (!EditLevelLayer::init(level)) return false;
+    bool init(GJGameLevel* level) {
+        if (!EditLevelLayer::init(level)) return false;
 
-		if (Mod::get()->getSettingValue<bool>("enable-editlevellayer")) {
-			auto shortcutButton = ShortcutsLayer::addShortcutButton(this);
-			auto menu = this->getChildByID("level-actions-menu");
-			menu->addChild(shortcutButton);
-			menu->updateLayout();
-		}
+        if (Mod::get()->getSettingValue<bool>("enable-editlevellayer")) {
+            auto shortcutButton = ShortcutsLayer::addShortcutButton(this);
+            auto menu = this->getChildByID("level-actions-menu");
+            menu->addChild(shortcutButton);
+            menu->updateLayout();
+        }
 
-		return true;
-	}
+        return true;
+    }
 };
 
 class $modify(CreatorLayer) {
-	bool init() {
-		if (!CreatorLayer::init()) return false;
+    bool init() {
+        if (!CreatorLayer::init()) return false;
 
-		if (Mod::get()->getSettingValue<bool>("enable-creatorlayer")) {
-			auto shortcutButton = ShortcutsLayer::addShortcutButton(
-				this, 1.f, CircleBaseColor::Green, CircleBaseSize::Small
-			);
-			auto menu = this->getChildByID("bottom-right-menu");
-			menu->addChild(shortcutButton);
-			menu->updateLayout();
-		}
+        if (Mod::get()->getSettingValue<bool>("enable-creatorlayer")) {
+            auto shortcutButton = ShortcutsLayer::addShortcutButton(
+                this, 1.f, CircleBaseColor::Green, CircleBaseSize::Small
+            );
+            auto menu = this->getChildByID("bottom-right-menu");
+            menu->addChild(shortcutButton);
+            menu->updateLayout();
+        }
 
-		return true;
-	}
+        return true;
+    }
 };
 
 class $modify(LevelBrowserLayer) {
-	bool init(GJSearchObject* p0) {
-		if (!LevelBrowserLayer::init(p0)) return false;
+    bool init(GJSearchObject* p0) {
+        if (!LevelBrowserLayer::init(p0)) return false;
 
-		if (Mod::get()->getSettingValue<bool>("enable-levelbrowserlayer")) {
-			auto shortcutButton = ShortcutsLayer::addShortcutButton(
-				this, 1.f, CircleBaseColor::Green, CircleBaseSize::Small
-			);
-			auto menu = this->getChildByID("back-menu");
-			auto backButton = menu->getChildByID("back-button");
-			menu->insertAfter(shortcutButton, backButton);
-			menu->updateLayout();
-		}
+        if (Mod::get()->getSettingValue<bool>("enable-levelbrowserlayer")) {
+            auto shortcutButton = ShortcutsLayer::addShortcutButton(
+                this, 1.f, CircleBaseColor::Green, CircleBaseSize::Small
+            );
+            auto menu = this->getChildByID("back-menu");
+            auto backButton = menu->getChildByID("back-button");
+            menu->insertAfter(shortcutButton, backButton);
+            menu->updateLayout();
+        }
 
-		return true;
-	}
+        return true;
+    }
 };
 
 class $modify(LevelSearchLayer) {
-	bool init(int p0) {
-		if (!LevelSearchLayer::init(p0)) return false;
+    bool init(int p0) {
+        if (!LevelSearchLayer::init(p0)) return false;
 
-		if (Mod::get()->getSettingValue<bool>("enable-levelsearchlayer")) {
-			auto shortcutButton = ShortcutsLayer::addShortcutButton(
-				this, 1.f, CircleBaseColor::Green, CircleBaseSize::Small
-			);
-			auto menu = CCMenu::create();
-			menu->setZOrder(1);
-			menu->setID("shortcut-menu"_spr);
-			menu->addChild(shortcutButton);
-			menu->setPosition(
-				shortcutButton->getContentSize() / 2 + CCPoint{8.f, 8.f}
-			);
-			this->addChild(menu);
-		}
+        if (Mod::get()->getSettingValue<bool>("enable-levelsearchlayer")) {
+            auto shortcutButton = ShortcutsLayer::addShortcutButton(
+                this, 1.f, CircleBaseColor::Green, CircleBaseSize::Small
+            );
+            auto menu = CCMenu::create();
+            menu->setZOrder(1);
+            menu->setID("shortcut-menu"_spr);
+            menu->addChild(shortcutButton);
+            menu->setPosition(
+                shortcutButton->getContentSize() / 2 + CCPoint{8.f, 8.f}
+            );
+            this->addChild(menu);
+        }
 
-		return true;
-	}
+        return true;
+    }
 };
 
 class $modify(LevelInfoLayer) {
-	bool init(GJGameLevel* level, bool p1) {
-		if (!LevelInfoLayer::init(level, p1)) return false;
+    bool init(GJGameLevel* level, bool p1) {
+        if (!LevelInfoLayer::init(level, p1)) return false;
 
-		if (Mod::get()->getSettingValue<bool>("enable-levelinfolayer")) {
-			auto shortcutButton = ShortcutsLayer::addShortcutButton(
-				this, 1.f, CircleBaseColor::Green, CircleBaseSize::Small
-			);
-			auto menu = this->getChildByID("back-menu");
-			auto backButton = menu->getChildByID("back-button");
-			menu->insertAfter(shortcutButton, backButton);
-			menu->updateLayout();
-		}
+        if (Mod::get()->getSettingValue<bool>("enable-levelinfolayer")) {
+            auto shortcutButton = ShortcutsLayer::addShortcutButton(
+                this, 1.f, CircleBaseColor::Green, CircleBaseSize::Small
+            );
+            auto menu = this->getChildByID("back-menu");
+            auto backButton = menu->getChildByID("back-button");
+            menu->insertAfter(shortcutButton, backButton);
+            menu->updateLayout();
+        }
 
-		return true;
-	}
+        return true;
+    }
 };
